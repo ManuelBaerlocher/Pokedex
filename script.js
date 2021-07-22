12dd1be7b3b40deef3df9eea21a9ac11c7e636c4
Manuelb1990 <manuell@bluewin.ch>
2021-07-22 22:00:08 +0200

    Searchbox finish, but Performance Problem! next style Single Side

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let pokedex
+let pokedex;
 let currentPokemon;
+let pokemons = [];
+
 
 
 async function loadPokemon(name) {
@@ -39,26 +41,42 @@ async function loadGeneralPokemon() {
         let pokemon = pokemonList[i]['pokemon_species'];
         let name = pokemon['name'];
 
-        namefomat = name[0].toUpperCase() + name.slice(1).toLowerCase();
+        nameformat = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
 
         await loadPokemon(name);
-        renderHtml(i, namefomat);
+        await fillPokemons(i, nameformat);
+        renderHtml(i, nameformat);
         renderHtmlgeneralClass(i);
         checkClass(i);
         loadImg(i, currentPokemon);
     }
 }
 
-function loadImg(i, currentPokemon){
-    document.getElementById('pokedexGeneralImg-' + i).src = currentPokemon['sprites']['other']['dream_world']['front_default'];
+function fillPokemons(i, nameformat) {
+    pokemons.push([i]);
+    pokemons[i] = {
+        'name': '',
+        'img': '',
+        'sort': [],
+    }
+    pokemons[i]['name'] = nameformat;
+}
+
+function loadImg(i, currentPokemon) {
+    
+    let pokemonimg = currentPokemon['sprites']['other']['dream_world']['front_default'];
+    document.getElementById('pokedexGeneralImg-' + i).src = pokemonimg;
+
+    pokemons[i]['img'] = pokemonimg;
 
 }
 
-function renderHtml(i, namefomat) {
+function renderHtml(i, nameformat) {
     document.getElementById('pokedexGeneralList').innerHTML += `
-    <div id="pokemonGeneral${i}" class="pokemonGeneral">
+    <div onclick="loadSingleView(${i})" id="pokemonGeneral${i}" class="pokemonGeneral">
         <div class="pokedexGeneralName">
-            <h3>${namefomat}</h3>
+            <h3>${nameformat}</h3>
         </div>
         <div class="listbottom">
             <div id="generalClass${i}"></div>
@@ -67,7 +85,7 @@ function renderHtml(i, namefomat) {
     </div>`;
 }
 
-function renderHtmlgeneralClass(i){
+function renderHtmlgeneralClass(i) {
     types = currentPokemon['types']
 
     for (let j = 0; j < types.length; j++) {
@@ -75,47 +93,167 @@ function renderHtmlgeneralClass(i){
 
         sortformat = sort[0].toUpperCase() + sort.slice(1).toLowerCase();
 
+        pokemons[i].sort.push(sortformat);
+
         if (document.getElementById(`generalClass${i}`).innerHTML == 0) {
             document.getElementById(`generalClass${i}`).innerHTML += `<div class="sort" id="sort${i}">${sortformat}</div>`
         } else {
             document.getElementById(`generalClass${i}`).innerHTML += `<div class="sort">${sortformat}</div>`
         }
-    } 
+    }
 }
 
 function checkClass(i) {
     let typclass = document.getElementById(`sort${i}`).innerHTML;
     let backgroundColor = document.getElementById(`pokemonGeneral${i}`);
 
-    if (typclass == 'Grass') {
-        backgroundColor.style.backgroundColor = "#7fdd6d";
-    } else if (typclass == 'Fire') {
-        backgroundColor.style.backgroundColor = "#ff5f6e";
-    } else if (typclass == 'Water') {
-        backgroundColor.style.backgroundColor = "#2d97eb";
-    } else if (typclass == 'Normal') {
-        backgroundColor.style.backgroundColor = "#9e9ca0";
-    } else if (typclass == 'Electric') {
-        backgroundColor.style.backgroundColor = "#e2c700";
-    } else if (typclass == 'Bug') {
-        backgroundColor.style.backgroundColor = "#a25757";
-    } else if (typclass == 'Poison') {
-        backgroundColor.style.backgroundColor = "#ffae03";
-    } else if (typclass == 'Ground') {
-        backgroundColor.style.backgroundColor = "#743b3b";
-    } else if (typclass == 'Fairy') {
-        backgroundColor.style.backgroundColor = "#9147bb";
-    } else if (typclass == 'Fighting') {
-        backgroundColor.style.backgroundColor = "#eb3434";
-    } else if (typclass == 'Psychic') {
-        backgroundColor.style.backgroundColor = "#7c1457";
-    } else if (typclass == 'Rock') {
-        backgroundColor.style.backgroundColor = "#7c1457";
-    } else if (typclass == 'Ghost') {
-        backgroundColor.style.backgroundColor = "#194600";
-    } else if (typclass == 'Ice') {
-        backgroundColor.style.backgroundColor = "#2accc6";
-    } else if (typclass == 'Dragon') {
-        backgroundColor.style.backgroundColor = "#836312";
+    backgroundColor.style.backgroundColor = getColorForClass(typclass);
+}
+
+
+
+function getColorForClass(typclass) {
+    switch (typclass) {
+        case 'Grass':
+            return '#7fdd6d';
+        case 'Fire':
+            return '#ff5f6e';
+        case 'Water':
+            return '#2d97eb';
+        case 'Normal':
+            return '#9e9ca0';
+        case 'Electric':
+            return '#e2c700';
+        case 'Bug':
+            return '#a25757';
+        case 'Poison':
+            return '#ffae03';
+        case 'Ground':
+            return '#743b3b';
+        case 'Fairy':
+            return '#9147bb';
+        case 'Fighting':
+            return '#eb3434';
+        case 'Psychic':
+            return '#eb3434';
+        case 'Rock':
+            return '#7c1457';
+        case 'Ghost':
+            return '#194600';
+        case 'Ice':
+            return '#194600';
+        case 'Dragon':
+            return '#836312';
     }
 }
+
+function searchPokemon() {
+
+    let searchinput = document.getElementById('searchInput').value;
+    searchinput = searchinput.toLowerCase();
+
+    document.getElementById('pokedexGeneralList').innerHTML = '';
+
+    for (let i = 0; i < pokemons.length; i++) {
+        let name = pokemons[i]['name'];
+
+        if (name.toLowerCase().includes(searchinput)) {
+            nameformat = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+            renderHtml(i, nameformat);
+            renderHtmlSearchImg(i);
+            renderHtmlSearchSort(i);
+            checkClass(i);
+        }
+    }
+
+}
+
+function renderHtmlSearchImg(i) {
+    let pokemonimg = pokemons[i]['img'];
+
+    document.getElementById('pokedexGeneralImg-' + i).src = pokemonimg;
+}
+
+function renderHtmlSearchSort(i) {
+    let pokemonsort = pokemons[i]['sort']
+
+    for (let j = 0; j < pokemonsort.length; j++) {
+        let sort = pokemons[i]['sort'][j];
+
+
+        if (document.getElementById(`generalClass${i}`).innerHTML == 0) {
+            document.getElementById(`generalClass${i}`).innerHTML += `<div class="sort" id="sort${i}">${sort}</div>`
+        } else {
+            document.getElementById(`generalClass${i}`).innerHTML += `<div class="sort">${sort}</div>`
+        }
+    }
+
+}
+
+async function loadSingleView(i) {
+    let pokemonList = pokedex['pokemon_entries'];
+    let pokemon = pokemonList[i]['pokemon_species'];
+    let name = pokemon['name'];
+
+
+
+    changeClasslist();
+    await loadPokemon(name);
+    renderHtmlSingleName(name);
+    renderHtmlSingleClass();
+    checkSingleClass();
+    renderHtmlSingleImg();
+    
+
+}
+
+function changeClasslist() {
+    document.getElementById('pokedexSingleView').classList.remove('d-none');
+    document.getElementById('pokedexGeneralView').classList.add('d-none');
+}
+
+function renderHtmlSingleName(name) {
+    let nameformat = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+    document.getElementById('pokemonName').innerHTML = nameformat;
+}
+
+function renderHtmlSingleImg(){
+    let pokemonimg = currentPokemon['sprites']['other']['dream_world']['front_default'];
+    document.getElementById('pokemonImg').src = pokemonimg;
+
+}
+
+function renderHtmlSingleClass() {
+    types = currentPokemon['types'];
+
+    document.getElementById(`pokemonSort`).innerHTML = '';
+
+    for (let j = 0; j < types.length; j++) {
+        let sort = types[j]['type']['name'];
+
+        sortformat = sort[0].toUpperCase() + sort.slice(1).toLowerCase();
+
+        if (document.getElementById(`pokemonSort`).innerHTML == 0) {
+            document.getElementById(`pokemonSort`).innerHTML += `<div class="sort" id="singleSort">${sortformat}</div>`
+        } else {
+            document.getElementById(`pokemonSort`).innerHTML += `<div class="sort">${sortformat}</div>`
+        }
+    }
+}
+
+function checkSingleClass() {
+    let typclass = document.getElementById('singleSort').innerHTML;
+    let backgroundColor = document.getElementById('pokedexSingleView');
+
+    backgroundColor.style.backgroundColor = getColorForClass(typclass);
+}
+
+
+function loadGeneralView() {
+    document.getElementById('pokedexSingleView').classList.add('d-none');
+    document.getElementById('pokedexGeneralView').classList.remove('d-none');
+}
+
+
